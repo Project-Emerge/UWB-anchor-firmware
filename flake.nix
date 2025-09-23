bc4dfae8204f879e72b476424d0f1dbe2d4d12b1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-09-23 16:07:12 +0200

    chore: remove devenv in favor of direnv

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "UWB-anchor-firmware development environment";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "thumbv7em-none-eabihf" ];
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+            cargo-binutils
+            probe-rs
+            openocd
+            gdb
+            direnv
+            nix-direnv
+            openssl
+            pkg-config
+          ];
+          
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          OPENSSL_DIR = "${pkgs.openssl.dev}";
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        };
+      });
+}
