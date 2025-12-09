d4faeacbf915c8145e05a724389d3b5b12705d21
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2025-12-09 19:47:21 +0100

    feat: remove USB logging

--- src/main.rs
+++ src/main.rs
@@ -71,54 +71,6 @@ async fn main(_spawner: Spawner) {
     let mut charger_en1 = Output::new(p.PB0, Level::Low, Speed::Low);
     let mut charger_en2 = Output::new(p.PB1, Level::Low, Speed::Low);
 
-    // Create the driver, from the HAL.
-    let ep_out_buffer = [0u8; 256];
-
-    let driver = Driver::new(p.USB, Irqs, p.PA12, p.PA11);
-
-    // Create embassy-usb Config
-    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
-    config.max_packet_size_0 = 64;
-    config.manufacturer = Some("Embassy");
-    config.product = Some("USB-serial example");
-    config.serial_number = Some("12345678");
-    config.max_power = 100;
-
-    // Set the device class, subclass, and protocol.
-    config.device_class = 0xff;
-    config.device_sub_class = 0;
-    config.device_protocol = 0;
-
-    // The default is composite with IADs, which gives use device class code
-    // 0xEF, with is a miscellaneous device.
-    config.composite_with_iads = false;
-
-    // Create embassy-usb DeviceBuilder using the driver and config.
-    // It needs some buffers for building the descriptors.
-    let mut config_descriptor = [0; 256];
-    let mut bos_descriptor = [0; 256];
-    let mut control_buf = [0; 64];
-
-    let mut state = State::new();
-
-    let mut builder = Builder::new(
-        driver,
-        config,
-        &mut config_descriptor,
-        &mut bos_descriptor,
-        &mut [], // no msos descriptors
-        &mut control_buf,
-    );
-
-    // Create classes on the builder.
-    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
-    // Build the builder.
-    let mut usb = builder.build();
-    // Run the USB device.
-    let usb_fut = usb.run();
-
-    // class.wait_connection().await;
-
     // Bootstrap the STM6600 power management IC
     let power_int = ExtiInput::new(p.PB7, p.EXTI7, Pull::Up);
     let ps_hold = Output::new(p.PB5, Level::Low, Speed::Low);
@@ -161,24 +113,9 @@ async fn main(_spawner: Spawner) {
     charger_en1.set_high();
     charger_en2.set_low();
 
-    // Do stuff with the class!
-    let echo_fut = async {
-        loop {
-            info!("Connected");
-            class.wait_connection().await;
-            class
-                .write_packet(b"Hello from Embassy!\r\n")
-                .await
-                .expect("Can't send message");
-            let _ = echo(&mut class).await;
-            // info!("Disconnected");
-            Timer::after(Duration::from_millis(200)).await;
-        }
-    };
-
-    // Run everything concurrently.
-    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, echo_fut).await;
+    loop {
+        Timer::after(Duration::from_millis(100)).await;
+    }
 }
 
 struct Disconnected {}
